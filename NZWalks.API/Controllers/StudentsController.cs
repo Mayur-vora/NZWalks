5286fcafbae7e7b4bd08848dc0bec3b0ee0f8a75
Mayur-vora <mvora460@gmail.com>
2024-02-21 12:54:45 -0600

    added students controller

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // Get : https://localhost:port_number/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentnames = new string[] { "John", "Kushal", "May", "Diya", "Dev" };
+
+            return Ok(studentnames);
+        }
+    }
+}
