a5e761390ef656d6ee2241157dba1febb4a3c475
Mayur-vora <mvora460@gmail.com>
2024-02-21 16:26:53 -0600

    created sample get values for get all method

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:port_number/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        //GET ALL REGIONS
+        //GET: https://localhost:port_number/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Aukland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://www.leeabbamonte.com/wp-content/uploads/2016/02/IMG_4179.jpg"
+                },
+                new Region
+                {
+                     Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.wallpapers-for-desktop.eu/desktop/8203-panorama-city-new-of-wellington-8203-zeland.jpg"
+                },
+
+            };
+
+            return Ok(regions);
+        }
+    }
+}
