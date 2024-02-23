5787a6890e0ccad7044849a4efdd58dfc9a71358
Mayur-vora <mvora460@gmail.com>
2024-02-23 11:45:14 -0600

    added Put endpoint to the controller to edit and create regions in region controller

--- NZWalks.API/Models/DTO/RegionDto.cs
+++ NZWalks.API/Models/DTO/RegionDto.cs
@@ -3,8 +3,8 @@
     public class RegionDto
     {
         public Guid Id { get; set; }
-        public string Code { get; set; }
+        public String Code { get; set; }
         public String Name { get; set; }
-        public string? RegionImageUrl { get; set; }
+        public String? RegionImageUrl { get; set; }
     }
 }
