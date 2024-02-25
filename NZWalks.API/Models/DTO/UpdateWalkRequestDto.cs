a3c475c41098226f27f8c3aa6906cd9cfbc1b0e5
Mayur-vora <mvora460@gmail.com>
2024-02-25 17:58:18 -0600

    Modified the code for custom validators

--- NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
+++ NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
@@ -5,10 +5,12 @@ namespace NZWalks.API.Models.DTO
     public class UpdateWalkRequestDto
     {
         [Required]
+        [MinLength(3, ErrorMessage = "Name must not be less than 3 Characters")]
         [MaxLength(100, ErrorMessage = "Name must not be more than 100 Characters")]
         public String Name { get; set; }
         [Required]
-        [MaxLength(1000, ErrorMessage = "Name must not be more than 1000 Characters")]
+        [MinLength(5, ErrorMessage = "Description must not be less than 5 Characters")]
+        [MaxLength(1000, ErrorMessage = "Description must not be more than 1000 Characters")]
         public String Description { get; set; }
         [Required]
         [Range(1, 50, ErrorMessage = "The length of the walk must be between 1 to 50 Kilometres")]
