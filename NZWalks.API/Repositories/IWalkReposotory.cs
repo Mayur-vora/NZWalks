e9c4158a940664376f7e979b821751ded7a2c15a
Mayur-vora <mvora460@gmail.com>
2024-02-27 23:26:53 -0600

    Added sorting to Name And Length properties in walksController

--- NZWalks.API/Repositories/IWalkReposotory.cs
+++ NZWalks.API/Repositories/IWalkReposotory.cs
@@ -5,7 +5,8 @@ namespace NZWalks.API.Repositories
     public interface IWalkReposotory
     {
         Task <Walk> CreateAsync(Walk walk);
-        Task <List<Walk>> GetAllAsync(String? filterOn = null , String? filterQuery = null);
+        Task <List<Walk>> GetAllAsync(String? filterOn = null , String? filterQuery = null, 
+            String? sortBy = null, bool isAscending = true);
         Task <Walk?> GetByIdAsync(Guid id);
         Task <Walk?> UpdateAsync(Guid id, Walk walk);
         Task <Walk?> DeleteAsync(Guid id);
